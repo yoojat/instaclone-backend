4518c24cc78cceb3e623649d08ac43235fa9a567
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-06-23 22:23:37 +0900

    #3 BACKEND SETUP

---
+++ movies/movies.queries.js
@@ -0,0 +1,9 @@
+import client from '../client';
+
+
+export default {
+    Query: {
+        movies: () => client.movie.findMany(),
+        movie : (_,{id}) => client.movie.findUnique({where:{id}})
+      }
+}
